192c229cd76c2cdb8c2fcb05784f81f14c4df4c1
Success Enehikhuere <enehikhueresuccess@gmail.com>
2024-01-31 22:50:54 +0100

    feat:"set up four more components"

---
+++ src/components/paginationContainer.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const paginationContainer = () => {
+  return (
+    <div>paginationContainer</div>
+  )
+}
+
+export default paginationContainer
---
+++ src/components/productsContainer.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const productsContainer = () => {
+  return (
+    <div>productsContainer</div>
+  )
+}
+
+export default productsContainer
