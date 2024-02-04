8a2207473e7694965934dbae686eb03d4612a6ce
Success Enehikhuere <enehikhueresuccess@gmail.com>
2024-02-04 22:49:35 +0100

    feat: "set up clearCart, editItem, and removeItem reducers in the cartSlice"

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -22,50 +22,49 @@ const cartSlice = createSlice({
       const { product } = action.payload
       const item = state.cartItems.find((i) => i.cartID === product.cartID)
       if (item) {
-        item.productAmount =+ product.productAmount
+        item.amount += product.amount
       } else {
         state.cartItems.push(product)
       }
-      state.numItemsInCart += product.productAmount
-      state.cartTotal += product.price * product.productAmount
+      state.numItemsInCart += product.amount
+      state.cartTotal += product.price * product.amount
       cartSlice.caseReducers.calculateTotals(state)
       toast.success('item added to cart successfully!')
     },
 
     clearCart: (state) => {
       localStorage.setItem('cart', JSON.stringify(defaultState))
-      return defaultState;
+      return defaultState
     },
 
     removeItem: (state, action) => {
-      const {cartID} = action.payload
-      const product = state.cartItems.find((i)=>i.cartID === cartID)
-      state.cartItems.filter((i)=>i.cartID !== cartID)
+      const { cartID } = action.payload
+      const product = state.cartItems.find((i) => i.cartID === cartID)
+      state.cartItems = state.cartItems.filter((i) => i.cartID !== cartID)
 
-      state.numItemsInCart -= product.productAmount
-      state.cartTotal -= product.price * product.productAmount
+      state.numItemsInCart -= product.amount
+      state.cartTotal -= product.price * product.amount
       cartSlice.caseReducers.calculateTotals(state)
-      toast.error('item removed from cart!')
+      toast.error('Item removed from cart')
     },
 
-    editItem: () => {
-      const {cartID, productAmount} = state.payload
-      const item = state.cartItems.find((i)=>i.cartID===cartID)
-      state.numItemsInCart += productAmount - item.productAmount
+    editItem: (state, action) => {
+      const { cartID, amount } = action.payload
+      const item = state.cartItems.find((i) => i.cartID === cartID)
+      state.numItemsInCart += amount - item.amount
 
-      state.cartTotal += item.price * (productAmount - item.productAmount)
-      
-      item.ProductAmount = productAmount
+      state.cartTotal += item.price * (amount - item.amount)
+
+      item.amount = amount
+
       cartSlice.caseReducers.calculateTotals(state)
       toast.success('Cart updated')
-
     },
 
     calculateTotals: (state) => {
       state.tax = 0.1 * state.cartTotal
       state.orderTotal = state.cartTotal + state.shipping + state.tax
       localStorage.setItem('cart', JSON.stringify(state))
-      
     },
   },
 })
