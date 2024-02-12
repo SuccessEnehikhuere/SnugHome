89c5727e5ff5f910533a96222c98fcadc68b0537
Success Enehikhuere <enehikhueresuccess@gmail.com>
2024-02-12 02:37:07 +0100

    refactor: "update the toggle theme functionality, set up in the userSlice"

--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -1,22 +1,51 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { toast } from 'react-toastify'
 
-const initialState = ({
-  user: {username: 'Success'},
-  theme: 'winter'
-})
 
-export const userSlice = createSlice({
+const themes = {
+  winter: 'winter',
+  dracula: 'dracula',
+}
+
+
+const getThemeFromLocalStorage = () => {
+  const theme = localStorage.getItem('theme') || themes.winter
+  document.documentElement.setAttribute('data-theme', theme)
+  return theme
+}
+
+const getUserFromLocalStorage = () => {
+  return JSON.parse(localStorage.getItem('user')) || null
+}
+
+const initialState = {
+  user: getUserFromLocalStorage(),
+  theme: getThemeFromLocalStorage(),
+}
+
+
+
+
+const userSlice = createSlice({
   name: 'user',
   initialState,
   reducers: {
     loginUser: (state, action) => {
-      console.log('login')
+      const user = { ...action.payload.user, token: action.payload.jwt }
+      state.user = user,
+      localStorage.setItem('user',JSON.stringify(user));
     },
-    logoutUser: (state, action) => {
-      console.log('logout')
+    logoutUser: (state) => {
+      state.user = null;
+      localStorage.removeItem('user');
+      toast.success('Logged out successfully')
     },
-    toggleTheme: (state, action) => {
-      console.log('current theme')
+    toggleTheme: (state) => {
+      const { dracula, winter } = themes
+      state.theme = state.theme === dracula ? winter : dracula
+      document.documentElement.setAttribute('data-theme', state.theme)
+      localStorage.setItem('theme', state.theme)
+
     },
   },
 })
