9900a48abaf287326d4060334dcf9ed5df9b330a
Success Enehikhuere <enehikhueresuccess@gmail.com>
2024-02-16 16:55:11 +0100

    refactor: "change of code in index file"

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -12,7 +12,7 @@ export { default as ErrorElement } from './ErrorElement';
 // export { default as ProductsContainer } from './ProductsContainer';
 export {default as ProductsContainer} from './ProductsContainer'
 // export { default as PaginationContainer } from './PaginationContainer';
-export {default as PaginationContainer} from './PaginationContainer'
+export { default as PaginationContainer } from './PaginationContainer'
 export {default as Filters} from './Filters'
 export {default as Loading} from './Loading'
 export {default as CartTotals} from './CartTotals'
